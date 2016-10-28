bb52f0a1a28003f521f6d5b2315e662e22b865a5
RoiPerelman <RoiPerelman@gmail.com>
2016-10-28 18:33:54 +0300

    working food schedule checker - working with files, need to use redit instead. cant connect to mailgun with lambda function - need to fix

--- Lambda_schedule/call_mailgun.js
+++
@@ -1,4 +0,0 @@
-var email_feeder = require('./email_feeder.js');
-
-email_feeder.sendemail("warning", console.log);
-email_feeder.sendemail("normal", console.log);
---
+++ Lambda_schedule/food_schedule_check.js
@@ -0,0 +1,91 @@
+const aws = require('aws-sdk');
+var email_feeder = require('./email_feeder.js');
+
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+var bucket = "catfoodbucket";
+var src_key = "TimeSchedule.txt";
+var sleep_key = "StartFeeding.txt";
+
+const params = {
+    Bucket: bucket,
+    Key: src_key
+};
+
+const sleep_params = {
+    Bucket: bucket,
+    Key: sleep_key
+};
+
+const mode = {
+    SLEEP : 0,
+    EMAIL : 1,
+    EMAIL_SENT : 2,
+    NORMAL_FUNCTION : 3
+};
+
+var loop_status = mode.SLEEP;
+
+exports.handler = function(event, context, callback) {
+
+    console.log("at start our loop status is " + loop_status);
+
+    s3.getObject(sleep_params, function(err, data) {
+        if (err) {
+            loop_status = mode.SLEEP;
+            console.log("make StartFeeding.txt in bucket to Start Feeding");
+            callback(null, "make StartFeeding.txt in bucket to Start Feeding");
+        }
+        else {
+            s3.getObject(params, function(err, data) {
+                if (err) {
+                    loop_status = mode.SLEEP;
+                    console.log("No food in yet");
+                    callback(null, "No food in yet");
+                }
+                else {
+                    // change status to email in case of no feeding
+                    if(loop_status == mode.SLEEP) {
+                        loop_status = mode.EMAIL;
+                        console.log("status change from SLEEP to EMAIL");
+                    }
+                        
+                    var end = Math.floor(Date.now() / 60000);
+                    var start = Math.floor(Date.parse(data.LastModified) / 60000);
+        
+                    // for debug
+                    var time_difference = end - start
+                    console.log(time_difference);
+                    
+                    if(((time_difference > 15) || (time_difference < 0)) && (loop_status == mode.EMAIL)){
+                        email_feeder.sendemail("warning", console.log);
+                        loop_status = mode.EMAIL_SENT;
+                        console.log("email sent and status change to EMAIL.SENT");
+                        callback(null, "email sent");
+                    }
+                    else if(((time_difference > 30) || (time_difference < 0)) && (loop_status == mode.EMAIL_SENT)){
+                        loop_status = mode.SLEEP;
+                        s3.deleteObject(sleep_params, function(err, data) {
+                        if (err) 
+                            console.log(err);
+                        else     
+                            console.log("deleted StartFeeding.txt");
+                        });
+                        console.log("The cat has died!");
+                        callback(null, "The cat has died!");
+                    }
+                    else if((((end - start) <= 30) || ((end - start) >= 0)) && (loop_status == mode.EMAIL)){
+                        email_feeder.sendemail("normal", console.log);
+                        loop_status = mode.EMAIL_SENT;
+                        console.log("cat was fed in time")
+                        callback(null, "cat was fed in time")
+                    }
+                    else{
+                        console.log("cat was fed");
+                        callback(null, "cat was fed");
+                    }
+                }
+            });
+        }
+    });
+}
