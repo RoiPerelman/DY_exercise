40a61fc9d357b88e705f7d716825a80eba127f65
RoiPerelman <RoiPerelman@gmail.com>
2016-10-29 17:33:52 +0300

    changed function name to Lambda_schedule.js - mailgun works. added config.json file for ease of changing + generic code./global --version email_feeder update to decrease double code

---
+++ Lambda_schedule/Lambda_schedule.js
@@ -0,0 +1,64 @@
+var config = require("./config.json");
+const aws = require('aws-sdk');
+//var email_feeder = require('./email_feeder.js');
+
+const s3 = new aws.S3();
+
+const params = {
+    Bucket: config.bucket,
+    Key: config.src_key
+};
+
+const mode = {
+    EMAIL : 0,
+    EMAIL_SENT : 1,
+    NORMAL : 2
+};
+
+var loop_status = mode.EMAIL;
+
+exports.handler = function(event, context, callback) {
+
+    s3.getObject(params, function(err, data) {
+        if (err) {
+            callback(null, "No food in yet, make sure src_key exists");
+        }
+        else {
+            //get the time difference
+            var cur_time_min = Math.floor(Date.now() / 60000);
+            var file_time_min = Math.floor(Date.parse(data.LastModified) / 60000);
+            var time_difference = cur_time_min - file_time_min;
+
+            // for debug
+            console.log("cur_time_min " + cur_time_min);
+            console.log("file_time_min " + file_time_min);
+            console.log("difference " + time_difference);
+            
+            if((time_difference > 15) && (loop_status == mode.EMAIL)){
+                loop_status = mode.EMAIL_SENT;
+                email_feeder.sendemail("warning", console.log);
+                callback(null, "email sent, loop_status change to EMAIL.SENT");
+            }
+            else if((time_difference < 15 ) && (loop_status == mode.EMAIL_SENT)){
+                loop_status = mode.NORMAL;
+                email_feeder.sendemail("normal", console.log);
+                callback(null, "cat was fed after email was sent, back to normal");
+            }
+            // extra - will put the loop status and time difference into a file every min
+            var dst_params2 = {
+                Bucket: config.bucket,
+                Key: config.dst_key,
+                Body: "loop status: " + loop_status + ", time difference: " + time_difference
+            };
+
+            s3.putObject(dst_params2, function (err, data) {
+                if (err) {
+                    console.log("error with putting file");
+                }
+                else {
+                    callback(null, "passed key " + data.Key + " the body " + dst_params2.Body);
+                }
+            });
+        }
+    });
+}
--- Lambda_schedule/email_feeder.js
+++ Lambda_schedule/email_feeder.js
@@ -1,35 +1,26 @@
 'use strict';
 
+var config = require("./config.json");
 console.log('starting function');
 
 //exports.handler = function(event, context, callback) {
 function sendemail(input, callback){
-    var api_key = 'key-25031afbf60e307f938d00ad2afaecdf';
-    var domain = 'sandbox815e1b27eafb41a690e87997877abdea.mailgun.org';
+    var api_key = config.api_key;
+    var domain = config.domain;
     var mailgun = require('mailgun-js')({apiKey : api_key, domain : domain});
 
-    if (input == "warning"){
-        var data = {
-            from : "perelmanroi@gmail.com",
-            to : "roiperelman@gmail.com",
-            subject : "Cat food Warning",
-            text : "Warning - The Cat is HUNGRY! give the cat food!!!"
-        }
-    }
-    else if( input == "normal"){
-        var data = {
-            from : "perelmanroi@gmail.com",
-            to : "roiperelman@gmail.com",
-            subject : "cat food is Back-To-Normal",
-            text : "You have used your 1 and only warning, cat is back to normal! feed every 15 min!"
-        }
+    var data = {
+        from : config.from_mail,
+        to : config.to_mail,
+        subject : config["subject_" + input],
+        text : config["body_" + input]
     }
 
     mailgun.messages().send(data, function(err, body) {
         if (err)
-            callback(err);
+            callback("mailgun messege arror " + err);
         else
-            callback("success");
+            callback("mailgun messege " + input + " sent");
     })
 };
 
